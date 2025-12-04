39305d7e808f88f4137e7f66c7342117bb5eb840
Lewis-mbui <lewmugo14479@gmail.com>
2025-12-04 09:15:23 +0300

    make overlay div interactive

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const headerNav = document.querySelector('.js-header__nav');
 const toggleIcon = document.querySelector('.js-menu-icon');
+const overlayDiv = document.querySelector('.js-overlay');
 
 toggleIcon.addEventListener('click', () => {
+  headerNav.classList.toggle('toggled');
+});
+
+overlayDiv.addEventListener('click', () => {
   headerNav.classList.toggle('toggled');
 });
